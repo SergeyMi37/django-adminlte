1d8cf2b7d961818a59c7c0bc46ed5599246d5546
SergeyMi37 <Sergey.Mikhaylenko@gmail.com>
2023-10-07 11:46:30 +0300

    fix env.iris

--- appmsw/utl.py
+++ appmsw/utl.py
@@ -22,10 +22,18 @@ def get_env_appmsw(request,name=""):
     _url=os.environ.get("APPMSW_IRIS_URL")
     if _url:
         _i = json.loads(classMethodFooter(request,url=_url))
-        if name=="iris_footer":
+        #print(_i)
+        try:
+            if _i['status'] !='ok':
+                return _i['status']
+            if name=="iris_footer":
+                return _i
+            elif name=="iris_instance":
+                return _i['instance'].split("*")[1]
+            elif name=="iris_host":
+                return _i['host']
+        except Exception as err:
+            print("---err-classMethod--------",err)
+            _i = f'{{"status":"Error get_env_appmsw {err} for :{name}"}}'
             return _i
-        elif name=="iris_instance":
-            return _i['instance'].split("*")[1]
-        elif name=="iris_host":
-            return _i['host']
     return ""
